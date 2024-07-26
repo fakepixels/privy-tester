ea4be59c9ce3401bc60f117a9f89f1066776c7b5
fakepixels <tinaxhe@gmail.com>
2024-07-26 13:49:42 -0400

    logged in state

--- components/homepage.tsx
+++ components/homepage.tsx
@@ -18,13 +18,19 @@ function ConnectWalletButton() {
 }
 
 export function Homepage() {
+  const { ready, authenticated, user } = usePrivy();
+
   return (
-        <div className="flex min-h-screen flex-col items-center justify-center bg-background">
-          <header className="mb-8 text-center">
-            <h1 className="text-2xl font-bold text-primary">Privy & Coinbase Smart Wallet</h1>
-            <p className="text-sm">Default Smart Wallet implementation</p>
-          </header>
-          <ConnectWalletButton />
-        </div>
+    <div className="flex min-h-screen flex-col items-center justify-center bg-background">
+      <header className="mb-8 text-center">
+        <h1 className="text-2xl font-bold text-primary">Privy & Coinbase Smart Wallet</h1>
+        <p className="text-sm">Coinbase Smart Wallet implementation</p>
+      </header>
+      {ready && authenticated ? (
+        <p className="mb-4 text-md">You are succesfully logged in. User ID: {user?.id}</p>
+      ) : (
+        <ConnectWalletButton />
+      )}
+    </div>
   )
 }
